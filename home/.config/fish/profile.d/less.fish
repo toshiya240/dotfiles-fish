2a2e34a6897c2569b09e53c096164530c8151c7f
Toshiya NISHIO <toshiya240@gmail.com>
2018-12-01 14:04:22 +0900

    add LESS_TERMCAP

--- home/.config/fish/profile.d/less.fish
+++ home/.config/fish/profile.d/less.fish
@@ -9,3 +9,18 @@ if type -q src-hilite-lesspipe.sh
 else if type -q lesspipe.sh
     set -x LESSOPEN '| lesspipe.sh %s'
 end
+
+set -x LESS_TERMCAP_mb (tput bold; tput setaf 2)
+set -x LESS_TERMCAP_md (tput bold; tput setaf 6)
+set -x LESS_TERMCAP_me (tput sgr0)
+set -x LESS_TERMCAP_so (tput bold; tput setaf 3; tput setab 4)
+set -x LESS_TERMCAP_se (tput rmso; tput sgr0)
+set -x LESS_TERMCAP_us (tput smul; tput bold; tput setaf 7)
+set -x LESS_TERMCAP_ue (tput rmul; tput sgr0)
+set -x LESS_TERMCAP_mr (tput rev)
+set -x LESS_TERMCAP_mh (tput dim)
+set -x LESS_TERMCAP_ZN (tput ssubm)
+set -x LESS_TERMCAP_ZV (tput rsubm)
+set -x LESS_TERMCAP_ZO (tput ssupm)
+set -x LESS_TERMCAP_ZW (tput rsupm)
+set -x GROFF_NO_SGR 1
